553e020dd88761a0697c1d24ccca20d26a4a30c7
NazifaNeloy <241569685+NazifaNeloy@users.noreply.github.com>
2026-03-07 14:11:33 +0600

    Implement login authentication logic

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
- console.log("App Started");
+
+const loginForm = document.getElementById('loginForm');
+if (loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const u = document.getElementById('username').value;
+        const p = document.getElementById('password').value;
+        
+        if (u === 'admin' && p === 'admin123') {
+            localStorage.setItem('isLoggedIn', 'true');
+            window.location.href = 'dashboard.html';
+        } else {
+            alert('Invalid Credentials');
+        }
+    });
+}
